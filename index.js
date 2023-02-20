b00907f3d35f6dccba15c7babe6e1a745c46fc44
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2023-02-20 10:04:15 +0000

    started writing javascript

---
+++ index.js
@@ -0,0 +1,11 @@
+const inputItem = document.getElementById("getItem")
+const toDoList = document.getElementById("to-do-list")
+const addButton = document.getElementById("add-button")
+
+
+addButton.addEventListener("click", () => {
+
+    const inputItemText = inputItem.value;
+
+    const newItem = createElement('li')
+})
